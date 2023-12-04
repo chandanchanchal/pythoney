ffa3d0f1380bb09ea45665cc9b7d87cfcc38e14d
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2023-12-04 10:34:53 +0530

    Create Person.py

---
+++ OOPS/Person.py
@@ -0,0 +1,24 @@
+class Person:
+    def __init__(self, name, sex, profession):
+        # data members (instance variables)
+        self.name = name
+        self.sex = sex
+        self.profession = profession
+
+    # Behavior (instance methods)
+    def show(self):
+        print('Name:', self.name, 'Sex:', self.sex, 'Profession:', self.profession)
+
+    # Behavior (instance methods)
+    def work(self):
+        print(self.name, 'working as a', self.profession)
+
+
+p1 = Person('person1', 'Male', 'SSE')
+p2 = Person('person2', 'Male', 'TL')
+
+p1.show();
+p1.work();
+
+p2.show();
+p2.work();
