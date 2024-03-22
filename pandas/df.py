dcc7a668c40a8d0212bc152ee130a38fe426439a
chandan chanchanl <34713852+chandanchanchal@users.noreply.github.com>
2024-03-22 15:59:49 +0530

    Create df.py

---
+++ pandas/df.py
@@ -0,0 +1,6 @@
+dataf = pd.DataFrame([
+    ['John Smith','123 Main St',34],
+    ['Jane Doe', '456 Maple Ave',28],
+    ['Joe Schmo', '789 Broadway',51]
+    ],
+    columns=['name','address','age'])
